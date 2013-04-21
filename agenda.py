9122f4cc45f56a802bfba84c2cde18bd906ccd1d
Eduardo Ramírez <ejramire@uc.cl>
2013-04-21 18:46:11 -0400

    top

--- agenda.py
+++ agenda.py
@@ -1,8 +1,8 @@
 from proceso import *
 
-class AddContact(Process):
+class NewContact(Process):
 	def __init__(self, attributes):
-		super(AddContact, self).__init__(self, attributes)
+		Process.__init__(self, attributes)
 		self.totalTime = 0
 		self.contactName = attributes[4]
 		self.contactNumber = attributes[5]
